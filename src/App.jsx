b0a75a57d947a5d1fd335c46f6deafcbf8b9bdd7
Shamim Alam <shamimalam4949@gmail.com>
2026-05-12 12:35:28 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Hero from './components/Hero/Hero';
 import Services from './components/Services/Services';
 import Banner from './components/Banner/Banner';
@@ -10,28 +10,38 @@ import Blog from './components/Blog/Blog';
 import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
-      <Navbar />
+      <Navbar isOpen={isOpen} setIsOpen={setIsOpen} />
+
       <main className="overflow-x-hidden bg-white text-dark">
         <section id="home">
           <Hero />
         </section>
+
         <section id="about">
           <About />
         </section>
+
         <section id="blog">
           <Blog />
         </section>
+
         <section id="services">
           <Services />
         </section>
+
         <Banner />
+
         <section id="subscribe">
           <Subscribe />
         </section>
+
         <Banner2 />
       </main>
+
       <Footer />
     </>
   );
